7ed779c318c376ee0e9614519ef9e00e49f5917f
Sarun <pond-srn@hotmail.com>
2019-06-23 22:54:14 +0700

    [Added]: add method for post data to repository.

--- java-homework/src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ java-homework/src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -5,8 +5,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.wongnai.interview.movie.Movie;
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+import com.wongnai.interview.movie.external.MoviesResponse;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +21,14 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		MoviesResponse movieResp = movieDataService.fetchAll();
+		Movie movie;
+		for (int i = 0; i < movieResp.size(); i++) {
+			movie = new Movie(movieResp.get(i).getTitle());
+			movie.setId(Long.valueOf(i));
+			movie.setName(movieResp.get(i).getTitle());
+			movie.getActors().addAll(movieResp.get(i).getCast());
+			movieRepository.save(movie);
+		}
 	}
 }
