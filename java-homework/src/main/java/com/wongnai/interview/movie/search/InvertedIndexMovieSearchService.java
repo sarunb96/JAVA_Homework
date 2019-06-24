3785f93daac4c20f021e90457b5f4da2c44fc77a
Sarun <pond-srn@hotmail.com>
2019-06-25 02:07:07 +0700

    [Added]: add function for query by invert index.

--- java-homework/src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
+++ java-homework/src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
@@ -1,7 +1,9 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
+import java.util.TreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
@@ -34,7 +36,50 @@ public class InvertedIndexMovieSearchService implements MovieSearchService {
 		// from inverted index for Star and for War so that you get movie ids 1,5,8 for Star and 2,5 for War. The result that
 		// you have to return can be union or intersection of those 2 sets of ids.
 		// By the way, in this assignment, you must use intersection so that it left for just movie id 5.
+		TreeMap<String, List<Long>> invertIndex = new TreeMap<String, List<Long>>(String.CASE_INSENSITIVE_ORDER);
+		Iterable<Movie> movie = movieRepository.findAll();
+		String[] term;
+		List<Long> ids = new ArrayList<Long>();
+		List<Long> movieIds = new ArrayList<Long>();
+		for (Movie movieData : movie) {
+			term = movieData.getName().split(" ");
+			ids.add(movieData.getId());
+			for (int i = 0; i < term.length; i++) {
+				term[i] = term[i].replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+				if (invertIndex.containsKey(term[i])) {
+					if (!invertIndex.get(term[i]).containsAll(ids)) {
+						invertIndex.get(term[i]).addAll(ids);
+					}
+				} else {
+					invertIndex.put(term[i], ids);
+				}
+			}
+			ids = new ArrayList<Long>();
+		}
 
-		return null;
+		String[] movieName;
+		if (!queryText.equals("") || queryText != null) {
+			movieName = queryText.toLowerCase().split(" ");
+			// get all ID of search movie
+			for (int i = 0; i < movieName.length; i++) {
+				movieName[i] = movieName[i].replaceAll("[^A-Za-z0-9]", "");
+				if (invertIndex.containsKey(movieName[i])) {
+					ids.addAll(invertIndex.get(movieName[i]));
+				}
+			}
+
+			// check if ID duplicated
+			Long distinctCount = ids.parallelStream().distinct().count();
+			if (distinctCount != ids.size()) {
+				for (Long movieId : ids) {
+					if (Collections.frequency(ids, movieId) >= movieName.length) {
+						movieIds.add(movieId);
+					}
+				}
+			} else {
+				movieIds = ids;
+			}
+		}
+		return (List<Movie>) movieRepository.findAllById(movieIds);
 	}
 }
